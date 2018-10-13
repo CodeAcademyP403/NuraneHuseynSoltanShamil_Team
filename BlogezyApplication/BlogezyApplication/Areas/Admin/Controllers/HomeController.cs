3eaf008649ecc2e74ec4a34d17572239040646fa
shamilms <shamilms@code.edu.az>
2018-10-13 12:59:36 +0400

    Shamil_Area-ArticlePageEdited

--- BlogezyApplication/BlogezyApplication/Areas/Admin/Controllers/HomeController.cs
+++ BlogezyApplication/BlogezyApplication/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogezyApplication.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,22 @@ namespace BlogezyApplication.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController:Controller
     {
+        
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult AddArticle(AppUser appUser)
+        {
+            ViewBag.AppUserID = appUser.Id;
+            ViewBag.AppUserName = appUser.UserName;
+
+            return View(new Article);
+        }
+
+
     }
 }
