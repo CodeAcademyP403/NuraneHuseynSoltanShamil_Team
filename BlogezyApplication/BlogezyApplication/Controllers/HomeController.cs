f707a2280fca9bcaad294b9d24515b2dea852818
NuranaP <nurananp@code.edu.az>
2018-10-14 09:52:17 +0400

    HomrController Changed

--- BlogezyApplication/BlogezyApplication/Controllers/HomeController.cs
+++ BlogezyApplication/BlogezyApplication/Controllers/HomeController.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogezyApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogezyApplication.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly BlogezyDbContext context;
+        public HomeController(BlogezyDbContext context)
         {
-            return View();
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuElements = await context.Menus
+                                        .Where(x => x.Visibility == true)
+                                            .ToListAsync();
+
+            var articles = await context.Articles
+                                    .Where(x => x.Visibility == true)
+                                        .ToListAsync();
+            Common common = new Common()
+            {
+                Menus = menuElements,
+                Articles=articles
+            };
+
+            return View(common);
         }
     }
 }
